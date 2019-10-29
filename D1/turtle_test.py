92b6cdd9506cf4eae6cf6cdd311e99f963f1bd84
crawford96 <915666548@qq.com>
2019-10-29 11:39:09 +0800

    turtle_test_update

---
+++ D1/turtle_test.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.pensize(1)
+turtle.pencolor('blue')
+
+for i in range(360):
+    turtle.forward(1)
+    turtle.left(1)
+for i in range(360):
+    turtle.forward(1)
+    turtle.right(1)
+
+
+turtle.done()
